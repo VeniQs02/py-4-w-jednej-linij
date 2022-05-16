e0adebd5cd2b9cfa97e4f687b6b8be9eda4071cf
Jakub <jakub.kangowski7@vp.pl>
2022-05-16 13:15:29 +0200

    huj

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def board_printer(wid, hei, round_counter, board_array = []):
-    if round_counter == 0:
+def player_check(round_cr):
+    if round_cr % 2 == 0:
+        player_sign = 'O'
+    else:
+        player_sign = 'X'
+    return player_sign
+
+
+def board_printer(wid, hei, round_counter = 2, board_array = []):
+    if round_counter == 1:
         sign = ' '
         board_array = [[sign]*hei]*wid
 
@@ -9,29 +17,38 @@ def board_printer(wid, hei, round_counter, board_array = []):
         print('|')
     return board_array
 
-def pin_positioning(move, board_ar, round_cr):
-    hig = len(board_ar)
-    for i in range(hig-1):
-        if board_ar[move][hig-1] == ' ':
+
+def pin_positioning(move, board_ar, player_sign, height, width):
+    board_ar[2][3] = 'P'
+    board_printer(height, width)
+    for i in range(height):
+        #for j in range(move):
+        if board_ar[height - i][move-1] == ' ':
             continue
         else:
+            board_ar[height - i][move] = player_sign
+            break
 
-            board_ar[move][hig] = player_sign
+
 
 
 
 def gra(board_width, board_height):
-    round_counter, win_condition = 0
+    round_counter = 1
+    win_condition = 0
     board_array = board_printer(board_width, board_height, round_counter)
-    while win_condition == False:
+    while win_condition == 0:
         while True:
-            move = int(input('Podaj wartosc 1-7: '))
-            if move < 1 or move > 7:
-                continue
-        pin_positioning(move, board_array, round counter)
+            move = 5 #int(input('Podaj wartosc 1-7: '))
+            if move > 1 or move < 7:
+                break
+        player_sign = player_check(round_counter)
+        pin_positioning(move, board_array, player_sign, board_height, board_width)
         board_printer(board_width, board_height, round_counter, board_array)
+        #win_check()###
 
         round_counter += 1
 
+    print(f"Gracz {player_sign} wyrgywa!")
 
 gra(7, 6)
